2cf01ea28f436b3c7ec02f6ba6663cedeedfe65c
Oleksii-Davydov <109512999+Oleksii-Davydov@users.noreply.github.com>
2023-02-21 14:10:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,98 +1,81 @@
-
 const API = "https://jsonplaceholder.typicode.com";
-const controller = action => fetch(action)
-    .then(data => data.json())
-    .catch(error => alert(error.message))
+const controller = (action, collbak) => fetch(action)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => collbak(data))
+    .catch(error => alert(error))
 
 const form = document.querySelector(".form")
 const cardPublish = document.querySelector(".publish-container")
-const btnComments = document.createElement("button");
-
-
-
 
 form.addEventListener("submit", e => {
     e.preventDefault()
+
     function getPost() {
         const inputValue = document.getElementById("tentacles").value;
-        console.log(inputValue)
-        controller(`${API}/posts/${inputValue}`)
-            .then(data => publishPost(data));
+        controller(`${API}/posts/${inputValue}`, publishPost)
+
     }
 
     getPost()
 
 })
-
-// функция первая буква заглавная
 function ucFirst(str) {
     if (!str) return str;
     return str[0].toUpperCase() + str.slice(1);
 }
-
-// функция опубликовать на странице пост
 function publishPost(post) {
-    // создаем елементы
+    if (post === undefined) {
+        return
+    }
     const postTitle = document.createElement("h2");
     const postBody = document.createElement("p");
-    // const btnComments = document.createElement("button");
+    const btnComments = document.createElement("button");
+    btnComments.id = post.id
+    const commentsContainer = document.createElement("div");
+    commentsContainer.id = "comment-container" + post.id
 
-
-    // добавляем классы к елементам
     postTitle.classList.add("card-title");
     postBody.classList.add("card-body")
     btnComments.classList.add("card-btn")
-    btnComments.id = "btn-com"
-    // что именно лежит в елементе
+
     postTitle.innerText = ucFirst(post.title)
     postBody.innerText = ucFirst(post.body)
     btnComments.innerText = "Comments"
 
-    // добавляем во внутрь
     cardPublish.append(postTitle)
     cardPublish.append(postBody)
     cardPublish.append(btnComments)
-
-
-
-}
-
-
-
-// function addComments(comments) {
-   // const btnComments = document.createElement("button");
+    cardPublish.append(commentsContainer)
     btnComments.addEventListener("click", e => {
         e.preventDefault()
-        //const btnComments = document.createElement("button")
-        function getCommit() {
-            const inputValue = document.getElementById("tentacles").value
-            controller(`${API}/comments?postId=${inputValue}`)//.then(data => addComments(data) )
-                .then(data => {
-                    data.forEach(comment => addComments(comment));
+        function getComments() {
 
-                })
-                .catch(error => alert(error.message))
+            controller(`${API}/comments?postId=${e.target.id}`, addComments)
+
         }
 
-        getCommit()
+        getComments()
+
+        function addComments(comments) {
+            comments.forEach(comment => {
+                const authorComment = document.createElement("h4");
+                const commentBody = document.createElement("p");
+
+                authorComment.classList.add("author-comment");
+                commentBody.classList.add("comment-body")
+
+                authorComment.innerText = ucFirst(comment.name)
+                commentBody.innerText = ucFirst(comment.body)
+
+                commentsContainer.append(authorComment)
+                commentsContainer.append(commentBody)
+
+            })
+        }
     })
-
-function addComments(comments) {
-    // const btnComments = document.createElement("button");
-    const authorComment = document.createElement("h4");
-    const commentBody = document.createElement("p");
-
-    authorComment.classList.add("author-comment");
-    commentBody.classList.add("comment-body")
-
-    authorComment.innerText = ucFirst(comments.name)
-    commentBody.innerText = ucFirst(comments.body)
-
-    cardPublish.append(authorComment)
-    cardPublish.append(commentBody)
 }
-
-
-
-
-
